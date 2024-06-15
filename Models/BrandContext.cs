16a0db5ccbde9e737c5f3560295ca06c45dc7711
Cindy <ccchhh666888@gmail.com>
2024-06-15 19:31:25 +0700

    Add project files.

---
+++ Models/BrandContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace CrudOperationslnNetCore.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options) 
+        { 
+            
+        }
+
+        public DbSet <Brand> Brands { get; set; }
+
+    }
+}
